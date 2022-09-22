e0e0125f3850e84b6df641738ad1e1644ffeaa44
sjorgy3 <87779735+sjorgy3@users.noreply.github.com>
2022-09-21 22:09:30 -0600

    Commitation

--- IDAutomaton.h
+++ IDAutomaton.h
@@ -15,7 +15,8 @@ private:
     void S1(const std::string& input);
 
 public:
-    IDAutomaton() : Automaton(TokenType::ID) {}  // Call the base constructor
+    IDAutomaton() : Automaton(TokenType::ID
+    ) {}  // Call the base constructor
 
     void S0(const std::string& input);
 };
--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@ using namespace std;
 
 int main(int argc, char** argv) {
 
+    string filename = argv[1];
 
     Lexer* lexer = new Lexer();
 
